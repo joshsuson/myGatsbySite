65467492fe6a457261ba8c20a9e1b657c4dceda7
Josh Suson <josh.suson@gmail.com>
2020-05-08 12:38:27 -0400

    Added social links to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,22 @@
 import React from "react"
 import Form from "../components/form"
+import "../utils/fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-export default () => <Form />
+export default () => (
+  <div>
+    <Form />
+    <a href="">
+      <FontAwesomeIcon icon={["fab", "twitter-square"]} />
+    </a>
+    <a href="">
+      <FontAwesomeIcon icon={["fab", "github-square"]} />
+    </a>
+    <a href="">
+      <FontAwesomeIcon icon={["fab", "linkedin"]} />
+    </a>
+    <a href="">
+      <FontAwesomeIcon icon={["fab", "facebook-square"]} />
+    </a>
+  </div>
+)
