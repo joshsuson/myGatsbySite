dd2daac1728df6d43db7223a176bdb8e12b7dbeb
Josh Suson <josh.suson@gmail.com>
2020-05-15 15:13:01 -0400

    Adding drawer

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,6 +1,90 @@
-import React from "react"
-// import { Link } from "gatsby"
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import {
+  List,
+  ListItem,
+  Button,
+  Drawer,
+  ClickAwayListener,
+  Typography,
+} from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
 import "../utils/fontawesome"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-export default () => <FontAwesomeIcon icon="bars" />
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+    padding: 0,
+  },
+  listItem: {
+    padding: "16px",
+  },
+  listIcon: {
+    marginRight: "8px",
+    color: "#020202",
+  },
+  link: {
+    textDecoration: "none",
+  },
+  linkText: {
+    fontSize: "1rem",
+    color: "#020202",
+  },
+})
+
+export default function NavDrawer() {
+  const classes = useStyles()
+  const [left, setLeft] = useState(false)
+
+  const toggleDrawer = (anchor, open) => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return
+    }
+
+    setLeft(true)
+  }
+
+  const handleClickAway = () => {
+    setLeft(false)
+  }
+
+  const list = anchor => (
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <List component="nav" className={classes.list}>
+        <Link to="/" className={classes.link}>
+          <ListItem button divider={true} className={classes.listItem}>
+            <div className={classes.listIcon}>
+              <FontAwesomeIcon icon="home" />
+            </div>
+            <Typography variant="h6" className={classes.linkText}>
+              Home
+            </Typography>
+          </ListItem>
+        </Link>
+        <ListItem button divider={true}>
+          <FontAwesomeIcon icon="user" />
+          <Link to="/about">About Me</Link>
+        </ListItem>
+        <ListItem button divider={true}>
+          <FontAwesomeIcon icon="file-alt" />
+          <Link to="/projects">Projects</Link>
+        </ListItem>
+      </List>
+    </ClickAwayListener>
+  )
+
+  return (
+    <>
+      <Button onClick={toggleDrawer(left, true)}>
+        <FontAwesomeIcon icon="bars" />
+      </Button>
+      <Drawer anchor="left" open={left} onClose={toggleDrawer("left", false)}>
+        {list(left)}
+      </Drawer>
+    </>
+  )
+}
--- src/utils/fontawesome.js
+++ src/utils/fontawesome.js
@@ -1,5 +1,10 @@
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import {
+  faBars,
+  faHome,
+  faUser,
+  faFileAlt,
+} from "@fortawesome/free-solid-svg-icons"
 
-library.add(fab, faBars)
+library.add(fab, faBars, faHome, faUser, faFileAlt)
