f31317eea3c4080cc4275c7eceda4e2626e0e1c6
joshsuson <josh.suson@gmail.com>
2020-05-20 09:00:11 -0400

    Working on appbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,21 @@
 import React from "react"
 import Nav from "../components/nav"
+import { makeStyles } from '@material-ui/core/styles'
 import { AppBar, Typography, Toolbar } from "@material-ui/core"
 
+const useStyles = makeStyles({
+  title: {
+    alignSelf: center,
+  }
+})
+
 export default () => (
+  const classes = useStyles()
   <>
     <AppBar position="fixed">
       <Toolbar>
         <Nav />
-        <Typography>Josh Suson</Typography>
+        <Typography className={}>Josh Suson</Typography>
       </Toolbar>
     </AppBar>
     <Toolbar />
