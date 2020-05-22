75ca980a2d21e016e20a2b9faab0336394a9a76b
Josh Suson <josh.suson@gmail.com>
2020-05-22 19:47:50 -0400

    Started working on form

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,14 +1,8 @@
 import React from "react"
+import { TextField } from "@material-ui/core"
 
 export default () => (
   <form>
-    <div>
-      <input type="text" placeholder="First Name" name="firstName" />
-      <input type="text" placeholder="Last Name" name="lastName" />
-    </div>
-    <div>
-      <input type="email" placeholder="Enter your email" name="email" />
-      <textarea placeholder="Put your message here" name="message" />
-    </div>
+    <TextField id="firstname" variant="outlined" label="First Name" />
   </form>
 )
