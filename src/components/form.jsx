cc250851530572b203794e4b8adab55042f5985a
Josh Suson <josh.suson@gmail.com>
2020-05-22 22:31:09 -0400

    A little more form work

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,30 +3,28 @@ import { TextField } from "@material-ui/core"
 import { makeStyles } from "@material-ui/styles"
 
 const useStyles = makeStyles({
-	customForm: {
-		margin: "0 auto"
-	}
+  customForm: {
+    textAlign: "center",
+    width: "50%",
+    margin: "0 auto",
+  },
 })
 
 export default function FooterForm() {
-	const classes = useStyles()
-	return (
-		<form className={classes.customForm}>
-			<TextField
-				id="firstname"
-				variant="outlined"
-				label="First Name"
-			/>
-			<TextField
-				id="lastname"
-				variant="outlined"
-				label="Last Name"
-			/>
-			<TextField
-				id="email"
-				variant="outlined"
-				label="E-Mail"
-			/>
-		</form>
-	)
+  const classes = useStyles()
+  return (
+    <form className={classes.customForm}>
+      <TextField id="firstname" variant="outlined" label="First Name" />
+      <TextField id="lastname" variant="outlined" label="Last Name" />
+      <TextField id="email" variant="outlined" label="E-Mail" fullWidth />
+      <TextField
+        id="message"
+        variant="outlined"
+        label="Your Message"
+        multiline
+        fullWidth
+        rows={4}
+      />
+    </form>
+  )
 }
