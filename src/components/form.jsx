602e3204401a18447e934685a7537a04429e3895
joshsuson <josh.suson@gmail.com>
2020-05-23 02:08:15 +0000

    Working on form

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,8 +1,32 @@
 import React from "react"
 import { TextField } from "@material-ui/core"
+import { makeStyles } from "@material-ui/styles"
 
-export default () => (
-  <form>
-    <TextField id="firstname" variant="outlined" label="First Name" />
-  </form>
-)
+const useStyles = makeStyles({
+	customForm: {
+		margin: "0 auto"
+	}
+})
+
+export default function FooterForm() {
+	const classes = useStyles()
+	return (
+		<form className={classes.customForm}>
+			<TextField
+				id="firstname"
+				variant="outlined"
+				label="First Name"
+			/>
+			<TextField
+				id="lastname"
+				variant="outlined"
+				label="Last Name"
+			/>
+			<TextField
+				id="email"
+				variant="outlined"
+				label="E-Mail"
+			/>
+		</form>
+	)
+}
