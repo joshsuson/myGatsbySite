6b8268bfe16e9bd1634f5e6ad46234b44dcbb534
Josh Suson <josh.suson@gmail.com>
2020-05-24 12:41:15 -0400

    Working on footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,22 +1,48 @@
 import React from "react"
 import Form from "../components/form"
+import { makeStyles } from "@material-ui/core/styles"
 import "../utils/fontawesome"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-export default () => (
-  <div>
-    <Form />
-    <a href="">
-      <FontAwesomeIcon icon={["fab", "twitter-square"]} />
-    </a>
-    <a href="">
-      <FontAwesomeIcon icon={["fab", "github-square"]} />
-    </a>
-    <a href="">
-      <FontAwesomeIcon icon={["fab", "linkedin"]} />
-    </a>
-    <a href="">
-      <FontAwesomeIcon icon={["fab", "facebook-square"]} />
-    </a>
-  </div>
-)
+const useStyles = makeStyles({
+  customIcon: {
+    fontSize: "2.5em",
+    padding: "4px",
+  },
+})
+
+export default () => {
+  const classes = useStyles()
+
+  return (
+    <div>
+      <Form />
+      <div>
+        <a href="">
+          <FontAwesomeIcon
+            icon={["fab", "twitter-square"]}
+            className={classes.customIcon}
+          />
+        </a>
+        <a href="">
+          <FontAwesomeIcon
+            icon={["fab", "github-square"]}
+            className={classes.customIcon}
+          />
+        </a>
+        <a href="">
+          <FontAwesomeIcon
+            icon={["fab", "linkedin"]}
+            className={classes.customIcon}
+          />
+        </a>
+        <a href="">
+          <FontAwesomeIcon
+            icon={["fab", "facebook-square"]}
+            className={classes.customIcon}
+          />
+        </a>
+      </div>
+    </div>
+  )
+}
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,68 +1,83 @@
 import React from "react"
 import { TextField } from "@material-ui/core"
-import { makeStyles } from "@material-ui/styles"
+import { makeStyles } from "@material-ui/core/styles"
 import clsx from "clsx"
 
 const useStyles = makeStyles(theme => ({
-	customForm: {
-		textAlign: "center",
-		margin: "0 auto"
-		//	[theme.breakpoints.up("md")]: {
-		//		width: "50%"
-		//	}
-	},
-	nameField: {
-		width: "45%"
-	},
-	customInput: {
-		marginBottom: "16px"
-	},
-	lastNameInput: {
-		marginLeft: "8px"
-	},
-	customFullWidth: {
-		width: "92%"
-	}
+  customForm: {
+    textAlign: "center",
+    margin: "0 auto",
+    padding: "16px",
+    [theme.breakpoints.up("md")]: {
+      width: "50%",
+    },
+  },
+  nameField: {
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "45%",
+    },
+  },
+  customInput: {
+    marginBottom: "16px",
+  },
+  lastNameInput: {
+    [theme.breakpoints.up("md")]: {
+      marginLeft: "8px",
+    },
+  },
+  customFullWidth: {
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "92%",
+    },
+    [theme.breakpoints.up("lg")]: {
+      width: "91%",
+    },
+    [theme.breakpoints.up("xl")]: {
+      width: "90%",
+    },
+  },
 }))
 
 export default function FooterForm() {
-	const classes = useStyles()
-	const firstName = clsx(classes.customInput, classes.nameField)
-	const lastName = clsx(
-		classes.customInput,
-		classes.nameField,
-		classes.lastNameInput
-	)
-	const email = clsx(classes.customInput, classes.customFullWidth)
-	const message = clsx(classes.customInput, classes.customFullWidth)
-	return (
-		<form className={classes.customForm}>
-			<TextField
-				id="firstname"
-				variant="outlined"
-				label="First Name"
-				className={firstName}
-			/>
-			<TextField
-				id="lastname"
-				variant="outlined"
-				label="Last Name"
-				className={lastName}
-			/>
-			<TextField
-				id="email"
-				variant="outlined"
-				label="E-Mail"
-				className={email}
-			/>
-			<TextField
-				id="message"
-				variant="outlined"
-				label="Your Message"
-				multiline
-				rows={4}
-				className={message}
-			/>
-		</form>
-	)
+  const classes = useStyles()
+  const firstName = clsx(classes.customInput, classes.nameField)
+  const lastName = clsx(
+    classes.customInput,
+    classes.nameField,
+    classes.lastNameInput
+  )
+  const email = clsx(classes.customInput, classes.customFullWidth)
+  const message = clsx(classes.customInput, classes.customFullWidth)
+  return (
+    <form className={classes.customForm}>
+      <TextField
+        id="firstname"
+        variant="outlined"
+        label="First Name"
+        className={firstName}
+      />
+      <TextField
+        id="lastname"
+        variant="outlined"
+        label="Last Name"
+        className={lastName}
+      />
+      <TextField
+        id="email"
+        variant="outlined"
+        label="E-Mail"
+        className={email}
+      />
+      <TextField
+        id="message"
+        variant="outlined"
+        label="Your Message"
+        multiline
+        rows={4}
+        className={message}
+      />
+    </form>
+  )
 }
