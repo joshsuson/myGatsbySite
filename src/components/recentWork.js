59bcdda18fcc443982bbfdeb674b792433407515
Josh Suson <joshsuson@Joshs-MacBook-Pro.local>
2020-09-10 20:15:56 -0400

    Commenting out blog queries

--- src/components/recentWork.js
+++ src/components/recentWork.js
@@ -1,19 +1,19 @@
-import React from "react"
+// import React from "react"
 
-const RecentWork = props => {
-  return (
-    <>
-      <div>
-        {props.data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <h1>{node.frontmatter.title}</h1>
-            <h3>{node.frontmatter.description}</h3>
-            <p>{node.excerpt}</p>
-          </div>
-        ))}
-      </div>
-    </>
-  )
-}
+// const RecentWork = props => {
+//   return (
+//     <>
+//       <div>
+//         {props.data.allMarkdownRemark.edges.map(({ node }) => (
+//           <div key={node.id}>
+//             <h1>{node.frontmatter.title}</h1>
+//             <h3>{node.frontmatter.description}</h3>
+//             <p>{node.excerpt}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </>
+//   )
+// }
 
-export default RecentWork
+// export default RecentWork
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,32 +1,32 @@
 import React from "react"
 import Layout from "../templates/layout"
 import Treehouse from "../components/treehouse"
-import RecentWork from "../components/recentWork"
+// import RecentWork from "../components/recentWork"
 
 export default ({ data }) => {
   console.log(data)
   return (
     <Layout>
-      <RecentWork data={data} />
+      {/* <RecentWork data={data} /> */}
       <Treehouse />
     </Layout>
   )
 }
 
-export const query = graphql`
-  query {
-    allMarkdownRemark {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            description
-          }
-          excerpt
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query {
+//     allMarkdownRemark {
+//       totalCount
+//       edges {
+//         node {
+//           id
+//           frontmatter {
+//             title
+//             description
+//           }
+//           excerpt
+//         }
+//       }
+//     }
+//   }
+// `
