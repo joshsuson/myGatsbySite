f72918ff822d24ba669c8746f814bd8848951778
Josh Suson <joshsuson@Joshs-MacBook-Pro.local>
2020-09-10 20:07:57 -0400

    changing config

--- src/components/recentWork.js
+++ src/components/recentWork.js
@@ -3,13 +3,15 @@ import React from "react"
 const RecentWork = props => {
   return (
     <>
-      {props.data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <h1>{node.frontmatter.title}</h1>
-          <h3>{node.frontmatter.description}</h3>
-          <p>{node.excerpt}</p>
-        </div>
-      ))}
+      <div>
+        {props.data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h1>{node.frontmatter.title}</h1>
+            <h3>{node.frontmatter.description}</h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
