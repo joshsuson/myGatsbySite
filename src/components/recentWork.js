ddfc791c2499cc9bccce62f9ecb7e45b992e7dca
Josh Suson <joshsuson@Joshs-MacBook-Pro.local>
2020-09-13 18:38:50 -0400

    Working through adding thumbnail

--- src/components/recentWork.js
+++ src/components/recentWork.js
@@ -1,19 +1,21 @@
-// import React from "react"
+import React from "react"
+import Img from "gatsby-image"
 
-// const RecentWork = props => {
-//   return (
-//     <>
-//       <div>
-//         {props.data.allMarkdownRemark.edges.map(({ node }) => (
-//           <div key={node.id}>
-//             <h1>{node.frontmatter.title}</h1>
-//             <h3>{node.frontmatter.description}</h3>
-//             <p>{node.excerpt}</p>
-//           </div>
-//         ))}
-//       </div>
-//     </>
-//   )
-// }
+const RecentWork = props => {
+  return (
+    <>
+      <div>
+        {props.data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Img fluid={node.frontmatter.thumbnail.childImageSharp.fluid} />
+            <h1>{node.frontmatter.title}</h1>
+            <h3>{node.frontmatter.description}</h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
 
-// export default RecentWork
+export default RecentWork
--- src/data/content/blog/flourishing-finishes/index.md
+++ src/data/content/blog/flourishing-finishes/index.md
@@ -2,6 +2,6 @@
 title: Flourishing Finishes
 date: 2020-09-11T00:17:21.731Z
 description: A site for a small Stampin' Up Business
-thumbnail: screen-shot-2020-09-10-at-7.25.38-pm.png
+thumbnail: ff-screenshot.png
 ---
 I had to build a site for a small business. It was all about crafting and Stampin' Up.
